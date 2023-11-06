ca65a9d193cad88fadbc8c29a8a3794723db6043
Rafaelqueirozz <131413399+Rafaelqueirozz@users.noreply.github.com>
2023-11-06 18:44:31 -0300

    Atualização 06/11/2023 - feat: add jogo DetonaRalph

---
+++ 05_Construindo_Portfolio_Jogos_JavaScript/DetonaRalph/assets/scripts/engine.js
@@ -0,0 +1,71 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        currentTime: 31,
+        gameVelocity: 800,
+        hitPosition: 0,
+        result: 0
+    },
+    actions: {
+        timerId: null,
+        // timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000)
+    }
+};
+
+function playSound(audioName) {
+    let audio = new Audio(`./assets/audios/${audioName}.m4a`);
+    audio.volume = 0.07;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id
+};
+
+function moveEnemy () {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+};
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound('hit');
+            }
+        })
+    })
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+};
+
+function initialize () {
+    moveEnemy();
+    addListenerHitBox();
+};
+
+initialize();
